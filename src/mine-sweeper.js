476e12c1f62891b4caf728396b2206ad013701a1
Liliyafamiliya <lilyalapetina@gmail.com>
2023-12-12 22:50:51 +0400

    feat: done mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    const line = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      const perimeter = [];
+
+      if (matrix[i - 1]) {
+        const top = matrix[i - 1][j];
+        const leftTop = matrix[i - 1][j - 1];
+        const rigthTop = matrix[i - 1][j + 1];
+        perimeter.push(top, leftTop, rigthTop);
+      }
+
+      const right = matrix[i][j + 1];
+      const left = matrix[i][j - 1];
+
+      perimeter.push(right, left);
+
+      if (matrix[i + 1]) {
+        const bottom = matrix[i + 1][j];
+        const leftBottom = matrix[i + 1][j - 1];
+        const rightBottom = matrix[i + 1][j + 1];
+        perimeter.push(bottom, leftBottom, rightBottom);
+      }
+
+      const mines = perimeter.reduce((acc, item) => {
+        if (item) acc++;
+        return acc;
+      }, 0);
+      line.push(mines);
+    }
+    result.push(line);
+  }
+
+  return result;
 }
 
+minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+]);
+
 module.exports = {
-  minesweeper
+  minesweeper,
 };
