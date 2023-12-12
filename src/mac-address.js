2fe2ed36d498203202dbccee2ca8c2cf2d236d5f
Liliyafamiliya <lilyalapetina@gmail.com>
2023-12-12 22:49:19 +0400

    feat: done mac-address task

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  console.log(n);
+
+  const onlyLettesOrNumbers = n.split("-").join("");
+  console.log(onlyLettesOrNumbers);
+
+  if (
+    n[2] !== "-" ||
+    n[5] !== "-" ||
+    n[8] !== "-" ||
+    n[11] !== "-" ||
+    n[14] !== "-"
+  ) {
+    return false;
+  }
+
+  for (let i = 0; i < onlyLettesOrNumbers.length; i++) {
+    if (
+      Number.isNaN(+onlyLettesOrNumbers[i]) &&
+      (onlyLettesOrNumbers[i] < "A" || onlyLettesOrNumbers[i] > "F")
+    ) {
+      return false;
+    } else {
+      if (+onlyLettesOrNumbers[i] < 0 || +onlyLettesOrNumbers[i] > 9) {
+      }
+    }
+  }
+
+  return true;
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
